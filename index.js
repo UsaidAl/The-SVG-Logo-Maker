3e1e51ec416de87075d13e2807799a54a63edfb9
UsaidAl <ali_usaid98@hotmail.com>
2024-03-04 21:35:26 +0000

    SVG class and constructor for new SVG shapes and colors

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const inquirer = require('inquirer');
 const {Circle, Square, Triangle } = require("./lib/shapes")
 const {writeFile} = require("fs/promises")
-const {LogoText, SVG} = require("./lib/svg")
+const {LogoText, SVG} = require("./lib/svg");
+const { choices } = require('yargs');
 
 const questions = [
     {
@@ -26,3 +27,21 @@ const questions = [
         message: 'What colour would you like the text for your logo?',
     },
 ];
+
+    class SVG {
+        constructor() {
+            const logoText = new LogoText(choices.text, choices.textcolor);
+            switch (choices.shape){
+                case "circle":
+                    const circle = new Circle(choices.shapeColor);
+                    return new SVG(circle, logoText);
+                case "square":
+                        const square = new Square(choice.shapeColor);
+                        return new SVG(square, logoText);
+                case "triangle":
+                        const triangle = new Triangle(choices.shapeColor);
+                        return new SVG(triangle, logoText);
+
+            }
+        }
+    }
