8ca5c7db056a352076711f3a9373d794f98b1eb6
UsaidAl <ali_usaid98@hotmail.com>
2024-03-04 21:17:51 +0000

    questions

--- index.js
+++ index.js
@@ -3,3 +3,26 @@ const {Circle, Square, Triangle } = require("./lib/shapes")
 const {writeFile} = require("fs/promises")
 const {LogoText, SVG} = require("./lib/svg")
 
+const questions = [
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape for your logo:',
+        choices: ['circle', 'square', 'triangle'],
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'What colour would you like the logo?',
+    },
+    {
+        type: 'input',
+        name: 'text',
+        message: 'What 3 letters would you like on the logo?',
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'What colour would you like the text for your logo?',
+    },
+];
