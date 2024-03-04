86861410374de0dd631c6b85da74d6109426a4ad
UsaidAl <ali_usaid98@hotmail.com>
2024-03-04 21:09:12 +0000

    inquirer, writefile, shapes and SVG functions

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 const inquirer = require('inquirer');
+const {Circle, Square, Triangle } = require("./lib/shapes")
+const {writeFile} = require("fs/promises")
+const {LogoText, SVG} = require("./lib/svg")
+
