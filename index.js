e2780c712db77514f2218c4a3e48b9ceb462cd28
UsaidAl <ali_usaid98@hotmail.com>
2024-03-06 16:11:35 +0000

    [WITH TUTOR] inquirer prompts. if else statements for shapes and colors Variables and renders for svg

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
-const inquirer = require('inquirer');
-const {Circle, Square, Triangle } = require("./lib/shapes")
-const {writeFile} = require("fs/promises")
-const {LogoText, SVG} = require("./lib/svg");
-const { choices } = require('yargs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+
+const { Circle, Square, Triangle } = require("./lib/shapes");
+const { LogoText, SVG } = require("./lib/svg");
 
 const questions = [
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Choose a shape for your logo:',
-        choices: ['circle', 'square', 'triangle'],
-    },
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'What colour would you like the logo?',
-    },
-    {
-        type: 'input',
-        name: 'text',
-        message: 'What 3 letters would you like on the logo?',
-    },
-    {
-        type: 'input',
-        name: 'textColor',
-        message: 'What colour would you like the text for your logo?',
-    },
+  {
+    type: "list",
+    name: "shape",
+    message: "Choose a shape for your logo:",
+    choices: ["circle", "square", "triangle"],
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message: "What colour would you like the logo?",
+  },
+  {
+    type: "input",
+    name: "text",
+    message: "What 3 letters would you like on the logo?",
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "What colour would you like the text for your logo?",
+  },
 ];
 
-    class SVG {
-        constructor() {
-            const logoText = new LogoText(choices.text, choices.textcolor);
-            switch (choices.shape){
-                case "circle":
-                    const circle = new Circle(choices.shapeColor);
-                    return new SVG(circle, logoText);
-                case "square":
-                        const square = new Square(choice.shapeColor);
-                        return new SVG(square, logoText);
-                case "triangle":
-                        const triangle = new Triangle(choices.shapeColor);
-                        return new SVG(triangle, logoText);
+inquirer.prompt(questions).then((responses) => {
+  if (responses.shape === "circle") {
+      var shape = new Circle(responses.shapeColor);
+      
+  }
+  else if (responses.shape=== "triangle"){
+     var shape = new Triangle(responses.shapeColor)
+  }
+    else{var shape = new Square(responses.shapeColor)} 
+  var text = new LogoText(responses.text, responses.textColor);
+  var svg = new SVG(shape, text);
 
-            }
-        }
-    }
+  fs.writeFileSync(path.join(process.cwd(), "template.svg"), svg.render());
+});
